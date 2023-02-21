2c33ae9b3d9b606472de06d29a2d96c4454c108c
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-02-21 16:14:18 -0500

    Lab Complete

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Links(props){
+    return(
+     <div>
+        <h3>Links</h3>
+        <a href="https://www.linkedin.com/in/liza/"> {props.github} </a>
+        <a href= "https://www.github.com/liza"> {props.linkedin} </a>
+</div>   
+    
+)}
+
+export default Links
